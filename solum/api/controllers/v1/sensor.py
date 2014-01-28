38c2d3e2aed70376927f2df172d5770462463596
Pierre Padrixe <pierre.padrixe@numergy.com>
2014-01-28 14:28:37 +0100

    Add sensor datamodel and sensor handler
    Moved the sensor datamodel to a separate file and added sensor
    handler class. Also modified the sensor controller and unit test.
    This code is based on the new API worker architecture.
    
    Change-Id: I504634bb06391ab50b254fdf0f2a7f0f1bb36aee

--- solum/api/controllers/v1/sensor.py
+++ solum/api/controllers/v1/sensor.py
@@ -10,8 +10,6 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import datetime
-
 import pecan
 from pecan import rest
 import six
@@ -19,76 +17,10 @@ import wsme
 from wsme import types as wtypes
 import wsmeext.pecan as wsme_pecan
 
+from solum.api.controllers.v1.datamodel import sensor
+from solum.api.handlers import sensor_handler
+from solum.common import exception
 from solum.openstack.common.gettextutils import _
-
-from solum.api.controllers import common_types
-from solum.api.controllers.v1.datamodel import types as api_types
-
-
-SENSOR_TYPE = wtypes.Enum(str, 'str', 'float', 'int')
-
-
-class Sensor(api_types.Base):
-    """A Sensor resource represents exactly one supported sensor on one or
-    more resources. Sensor resources represent dynamic data about resources,
-    such as metrics or state. Sensor resources are useful for exposing data
-    that changes rapidly, or that may need to be fetched from a secondary
-    system.
-    """
-
-    documentation = common_types.Uri
-    "Documentation URI for the sensor."
-
-    target_resource = common_types.Uri
-    "Target resource URI to the sensor."
-
-    sensor_type = SENSOR_TYPE
-    "Sensor data type."
-
-    def get_value(self):
-        if self.sensor_type == 'int':
-            if int(self._value) != float(self._value):
-                raise ValueError(_('Value "%s" is not an integer.') %
-                                 str(self._value))
-            return int(self._value)
-        elif self.sensor_type == 'float':
-            return float(self._value)
-        else:
-            return str(self._value)
-
-    def set_value(self, value):
-        # Store the value as-is, because we don't know the order that the
-        # value and sensor_type are written, so convert to the desired type
-        # in get_value().
-        self._value = value
-
-    value = wtypes.wsproperty(str, get_value, set_value, mandatory=False)
-    "Value of the sensor."
-
-    timestamp = datetime.datetime
-    "Timestamp for Sensor."
-
-    operations_uri = common_types.Uri
-    "Operations URI for the sensor."
-
-    def __init__(self, **kwds):
-        self._value = '0'
-        super(Sensor, self).__init__(**kwds)
-
-    @classmethod
-    def sample(cls):
-        return cls(uri='http://example.com/v1/sensors/hb',
-                   name='hb',
-                   type='sensor',
-                   project_id='1dae5a09ef2b4d8cbf3594b0eb4f6b94',
-                   user_id='55f41cf46df74320b9486a35f5d28a11',
-                   description='A heartbeat sensor',
-                   documentation='http://example.com/docs/heartbeat/',
-                   target_resource='http://example.com/instances/uuid',
-                   sensor_type='str',
-                   value='30',
-                   timestamp=datetime.datetime.utcnow(),
-                   operations_uri='http://example.com:9777/operations/stop')
 
 
 class SensorController(rest.RestController):
@@ -98,14 +30,17 @@ class SensorController(rest.RestController):
         pecan.request.context['sensor_id'] = sensor_id
         self._id = sensor_id
 
-    @wsme_pecan.wsexpose(Sensor, wtypes.text)
+    @wsme_pecan.wsexpose(sensor.Sensor, wtypes.text)
     def get(self):
         """Return this sensor."""
-        error = _("Not implemented")
-        pecan.response.translatable_error = error
-        raise wsme.exc.ClientSideError(six.text_type(error))
+        try:
+            handler = sensor_handler.SensorHandler()
+            return handler.get(self._id)
+        except exception.SolumException as excp:
+            pecan.response.translatable_error = excp
+            raise wsme.exc.ClientSideError(six.text_type(excp), excp.code)
 
-    @wsme_pecan.wsexpose(Sensor, wtypes.text, body=Sensor)
+    @wsme_pecan.wsexpose(sensor.Sensor, wtypes.text, body=sensor.Sensor)
     def put(self, data):
         """Modify this sensor."""
         error = _("Not implemented")
@@ -129,14 +64,14 @@ class SensorsController(rest.RestController):
             remainder = remainder[:-1]
         return SensorController(sensor_id), remainder
 
-    @wsme_pecan.wsexpose(Sensor, body=Sensor, status_code=201)
+    @wsme_pecan.wsexpose(sensor.Sensor, body=sensor.Sensor, status_code=201)
     def post(self, data):
         """Create a new sensor."""
         error = _("Not implemented")
         pecan.response.translatable_error = error
         raise wsme.exc.ClientSideError(six.text_type(error))
 
-    @wsme_pecan.wsexpose([Sensor])
+    @wsme_pecan.wsexpose([sensor.Sensor])
     def get_all(self):
         """Return all sensors, based on the query provided."""
         return []
