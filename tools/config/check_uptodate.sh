6c8b90251f88bebaea46c7d516010916d8e29c36
Angus Salkeld <angus.salkeld@rackspace.com>
2014-01-29 09:39:42 +1000

    Update the config generator from oslo-incubator
    Update to sha 1ebf819e31dde74f4374c26a791458ccd25b29d9
    
    Note the scripts are also from oslo-incubator.
    
    Change-Id: I0c502f74cc79e594bfb838db9689997e169a9a86

--- tools/config/check_uptodate.sh
+++ tools/config/check_uptodate.sh
@@ -1,25 +1,25 @@
-#!/bin/sh
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
+#!/usr/bin/env bash
 
-TEMPDIR=`mktemp -d`
-CFGFILE=solum.conf.sample
-tools/config/generate_sample.sh -b ./ -p solum -o $TEMPDIR
-diff $TEMPDIR/$CFGFILE etc/solum/$CFGFILE
-same=$?
-rm -rf $TEMPDIR
-if [ $same -ne 0 ]
+PROJECT_NAME=${PROJECT_NAME:-solum}
+CFGFILE_NAME=${PROJECT_NAME}.conf.sample
+
+if [ -e etc/${PROJECT_NAME}/${CFGFILE_NAME} ]; then
+    CFGFILE=etc/${PROJECT_NAME}/${CFGFILE_NAME}
+elif [ -e etc/${CFGFILE_NAME} ]; then
+    CFGFILE=etc/${CFGFILE_NAME}
+else
+    echo "${0##*/}: can not find config file"
+    exit 1
+fi
+
+TEMPDIR=`mktemp -d /tmp/${PROJECT_NAME}.XXXXXX`
+trap "rm -rf $TEMPDIR" EXIT
+
+tools/config/generate_sample.sh -b ./ -p ${PROJECT_NAME} -o ${TEMPDIR}
+
+if ! diff -u ${TEMPDIR}/${CFGFILE_NAME} ${CFGFILE}
 then
-    echo "E: solum.conf.sample is not up to date, please run tools/config/generate_sample.sh"
-    exit 42
+   echo "${0##*/}: ${PROJECT_NAME}.conf.sample is not up to date."
+   echo "${0##*/}: Please run ${0%%${0##*/}}generate_sample.sh."
+   exit 1
 fi
