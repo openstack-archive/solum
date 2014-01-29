50358c4f6a9ad7546b72e7e1d6790c0f92c78930
Angus Salkeld <angus.salkeld@rackspace.com>
2014-01-29 13:15:28 +1000

    Add the alembic migration framework
    This includes a new binary "solum-db-manage".
    
    Change-Id: I55910c3b4c02ea89159e28266cb9927cbfbdc5a6

---
+++ solum/cmd/db_manage.py
@@ -0,0 +1,95 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Starter script for solum-db-manage."""
+
+import os
+
+from oslo.config import cfg
+
+from solum.openstack.common.db.sqlalchemy.migration_cli \
+    import manager as migration_manager
+from solum.openstack.common.db.sqlalchemy import session
+from solum.openstack.common import log as logging
+
+
+LOG = logging.getLogger(__name__)
+CONF = cfg.ConfigOpts()
+CONF.register_opts(session.sqlite_db_opts)
+CONF.register_opts(session.database_opts, 'database')
+
+
+def do_version(manager):
+    manager.version()
+
+
+def do_upgrade(manager):
+    manager.upgrade(CONF.command.revision)
+
+
+def do_downgrade(manager):
+    manager.downgrade(CONF.command.revision)
+
+
+def do_stamp(manager):
+    manager.stamp(CONF.command.revision)
+
+
+def do_revision(manager):
+    manager.revision(message=CONF.command.message,
+                     autogenerate=CONF.command.autogenerate)
+
+
+def add_command_parsers(subparsers):
+    parser = subparsers.add_parser('version')
+    parser.set_defaults(func=do_version)
+
+    parser = subparsers.add_parser('upgrade')
+    parser.add_argument('revision', nargs='?')
+    parser.set_defaults(func=do_upgrade)
+
+    parser = subparsers.add_parser('downgrade')
+    parser.add_argument('revision', nargs='?')
+    parser.set_defaults(func=do_downgrade)
+
+    parser = subparsers.add_parser('stamp')
+    parser.add_argument('revision', nargs='?')
+    parser.set_defaults(func=do_stamp)
+
+    parser = subparsers.add_parser('revision')
+    parser.add_argument('-m', '--message')
+    parser.add_argument('--autogenerate', action='store_true')
+    parser.set_defaults(func=do_revision)
+
+
+def get_manager():
+    alembic_path = os.path.join(os.path.dirname(__file__),
+                                '..', 'objects', 'sqlalchemy',
+                                'migration', 'alembic.ini')
+    migrate_path = os.path.join(os.path.dirname(__file__),
+                                '..', 'objects', 'sqlalchemy',
+                                'migration', 'alembic_migrations')
+    migration_config = {'alembic_ini_path': alembic_path,
+                        'migration_repo_path': migrate_path,
+                        'alembic_repo_path': migrate_path}
+    return migration_manager.MigrationManager(migration_config)
+
+
+def main():
+    command_opt = cfg.SubCommandOpt('command',
+                                    title='Command',
+                                    help='Available commands',
+                                    handler=add_command_parsers)
+    CONF.register_cli_opt(command_opt)
+    CONF(project='solum')
+    CONF.command.func(get_manager())
---
+++ solum/tests/test_db_manage.py
@@ -0,0 +1,68 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import sys
+
+import mock
+import testscenarios
+
+from solum.cmd import db_manage as cli
+from solum.tests import base
+
+
+load_tests = testscenarios.load_tests_apply_scenarios
+
+
+class TestCli(base.BaseTestCase):
+    scenarios = [
+        ('stamp', dict(
+            argv=['prog', 'stamp', 'foo'],
+            func_name='stamp',
+            exp_args=('foo',), exp_kwargs={})),
+        ('version', dict(
+            argv=['prog', 'version'],
+            func_name='version',
+            exp_args=[], exp_kwargs={})),
+        ('revision_auto', dict(
+            argv=['prog', 'revision', '--autogenerate', '-m' 'message'],
+            func_name='revision', exp_args=[],
+            exp_kwargs={'message': 'message',
+                        'autogenerate': True})),
+        ('revision', dict(
+            argv=['prog', 'revision', '-m', 'message'],
+            func_name='revision', exp_args=[],
+            exp_kwargs={'message': 'message',
+                        'autogenerate': False})),
+        ('upgrade', dict(
+            argv=['prog', 'upgrade', 'head'],
+            func_name='upgrade', exp_args=('head',),
+            exp_kwargs={})),
+        ('downgrade', dict(
+            argv=['prog', 'downgrade', 'folsom'],
+            func_name='downgrade', exp_args=('folsom',),
+            exp_kwargs={})),
+    ]
+
+    def setUp(self):
+        super(TestCli, self).setUp()
+        self.addCleanup(cli.CONF.reset)
+
+    @mock.patch.object(cli, 'get_manager')
+    def test_alembic_manager(self, mock_get_manager):
+        manager = mock_get_manager.return_value
+        with mock.patch.object(sys, 'argv', self.argv):
+            cli.main()
+            mock_get_manager.assert_called_once_with()
+            test_method = getattr(manager, self.func_name)
+            test_method.assert_called_once_with(*self.exp_args,
+                                                **self.exp_kwargs)
