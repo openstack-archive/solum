76f3c12acbc03acf501f990808560c6ad29115d7
Angus Salkeld <angus.salkeld@rackspace.com>
2014-03-03 15:32:05 +1000

    DB objects have destroy() not delete()
    sensors and extensions were calling delete instead of destroy
    
    Change-Id: I1cccdb03694d1678ecd358fa29c27a5f8b25d6d5

--- solum/tests/api/handlers/test_extension.py
+++ solum/tests/api/handlers/test_extension.py
@@ -67,6 +67,6 @@ class TestExtensionHandler(base.BaseTestCase):
         mock_registry.Extension.get_by_uuid.return_value = db_obj
         handler = extension.ExtensionHandler()
         handler.delete('test_id')
-        db_obj.delete.assert_called_once_with()
+        db_obj.destroy.assert_called_once_with(None)
         mock_registry.Extension.get_by_uuid.assert_called_once_with(None,
                                                                     'test_id')
