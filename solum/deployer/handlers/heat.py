d4cae895caf9ee3e6c25fca40dbf980fc468f29b
Ed Cranford <ed.cranford@rackspace.com>
2014-04-04 11:51:23 -0500

    Add configurable handler selection for Deployer
    Added to solum-deployer the configuration value 'handler'
    to select which one of the available modules in its
    handlers directory to use as its endpoint. The result is
    being able to configure exactly how images will be deployed.
    
    At present we only have default.py, which uses Heat to deploy
    a stack for the app, but in the future there may be other
    handlers to use another process.
    
    Since default.py was using Heat, I renamed it to heat.py and
    added a new default.py mainly for contract enforcement.
    
    Change-Id: I4c2aefc7d60e421c176bb8e854548486e9d800a3

---
+++ solum/deployer/handlers/heat.py
@@ -0,0 +1,125 @@
+# Copyright 2014 - Rackspace Hosting
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+"""Solum Deployer Heat handler."""
+
+import os
+import time
+import yaml
+
+from oslo.config import cfg
+from solum.common import clients
+from solum import objects
+from solum.openstack.common.gettextutils import _
+from solum.openstack.common import log as logging
+
+
+LOG = logging.getLogger(__name__)
+
+STATES = (PENDING, BUILDING, ERROR, READY, ERROR_STACK_CREATE_FAILED) = (
+    'PENDING', 'BUILDING', 'ERROR', 'READY', 'ERROR_STACK_CREATE_FAILED')
+
+OPT_GROUP = cfg.OptGroup(name='deployer',
+                         title='Options for the solum-deployer service')
+SERVICE_OPTS = [
+    cfg.IntOpt('max_attempts',
+               default=400,
+               help=('Number of attempts to query the Heat stack for '
+                     'finding out the status of the created stack and '
+                     'getting url of the DU created in the stack')),
+    cfg.IntOpt('wait_interval',
+               default=5,
+               help=('Sleep time interval between two attempts of querying '
+                     'the Heat stack. This interval is in seconds.')),
+]
+
+cfg.CONF.register_group(OPT_GROUP)
+cfg.CONF.register_opts(SERVICE_OPTS, OPT_GROUP)
+
+
+class Handler(object):
+    def __init__(self):
+        super(Handler, self).__init__()
+        objects.load()
+
+    def echo(self, ctxt, message):
+        LOG.debug(_("%s") % message)
+
+    def _get_template(self, template_flavor):
+        proj_dir = os.path.join(os.path.dirname(__file__), '..', '..', '..')
+        templ = os.path.join(proj_dir, 'etc', 'solum', 'templates',
+                             '%s.yaml' % template_flavor)
+        with open(templ) as templ_file:
+            template = templ_file.read()
+        return template
+
+    def deploy(self, ctxt, assembly_id, image_id):
+        # TODO(asalkeld) support template flavors (maybe an autoscaling one)
+        #                this could also be stored in glance.
+        template_flavor = 'basic'
+        template = self._get_template(template_flavor)
+
+        osc = clients.OpenStackClients(ctxt)
+
+        assem = objects.registry.Assembly.get_by_id(ctxt,
+                                                    assembly_id)
+
+        parameters = {'app_name': assem.name,
+                      'image': image_id}
+        created_stack = osc.heat().stacks.create(stack_name=assem.name,
+                                                 template=template,
+                                                 parameters=parameters)
+
+        assem.status = BUILDING
+        assem.save(ctxt)
+        stack_id = created_stack['stack']['id']
+
+        comp_description = 'Heat Stack %s' % (
+            yaml.load(template).get('description'))
+        objects.registry.Component.assign_and_create(ctxt, assem,
+                                                     'Heat Stack',
+                                                     comp_description,
+                                                     created_stack['stack']
+                                                     ['links'][0]['href'])
+
+        self._update_assembly_status(ctxt, assem, osc, stack_id)
+
+    def _update_assembly_status(self, ctxt, assem, osc, stack_id):
+        got_stack_status = False
+        for count in range(cfg.CONF.deployer.max_attempts):
+            stack = osc.heat().stacks.get(stack_id)
+            if stack.status == 'COMPLETE':
+                host_url = self._parse_server_url(stack)
+                if host_url is not None:
+                    assem.status = READY
+                    assem.application_uri = host_url
+                    assem.save(ctxt)
+                    got_stack_status = True
+                    break
+            elif stack.status == 'FAILED':
+                assem.status = ERROR
+                assem.save(ctxt)
+                got_stack_status = True
+                break
+            time.sleep(cfg.CONF.deployer.wait_interval)
+
+        if not got_stack_status:
+            assem.status = ERROR_STACK_CREATE_FAILED
+            assem.save(ctxt)
+
+    def _parse_server_url(self, heat_output):
+        """Parse server url from heat-stack-show output."""
+        if 'outputs' in heat_output._info:
+            return heat_output._info['outputs'][1]['output_value']
+        return None
---
+++ solum/tests/deployer/handlers/test_heat.py
@@ -0,0 +1,101 @@
+# Copyright 2014 - Rackspace Hosting
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import json
+import mock
+
+from solum.deployer.handlers import heat as heat_handler
+from solum.tests import base
+from solum.tests import fakes
+from solum.tests import utils
+
+
+class HandlerTest(base.BaseTestCase):
+    def setUp(self):
+        super(HandlerTest, self).setUp()
+        self.ctx = utils.dummy_context()
+
+    def test_create(self):
+        handler = heat_handler.Handler()
+        handler.echo = mock.MagicMock()
+        handler.echo({}, 'foo')
+        handler.echo.assert_called_once_with({}, 'foo')
+
+    @mock.patch('solum.deployer.handlers.heat.Handler._get_template')
+    @mock.patch('solum.objects.registry')
+    @mock.patch('solum.common.clients.OpenStackClients')
+    def test_deploy(self, mock_clients, mock_registry, mock_get_templ):
+        handler = heat_handler.Handler()
+
+        fake_assembly = fakes.FakeAssembly()
+        mock_registry.Assembly.get_by_id.return_value = fake_assembly
+        fake_template = json.dumps({'description': 'test'})
+        mock_get_templ.return_value = fake_template
+        mock_clients.return_value.heat.return_value.stacks.create.\
+            return_value = {"stack": {"id": "fake_id",
+                                      "links": [{"href": "http://fake.ref",
+                                                 "rel": "self"}]}}
+        handler._update_assembly_status = mock.MagicMock()
+        handler.deploy(self.ctx, 77, 'created_image_id')
+        parameters = {'image': 'created_image_id',
+                      'app_name': 'faker'}
+        mock_clients.return_value.heat.return_value.stacks.create.\
+            assert_called_once_with(stack_name='faker',
+                                    template=fake_template,
+                                    parameters=parameters)
+        assign_and_create_mock = mock_registry.Component.assign_and_create
+        assign_and_create_mock.assert_called_once_with(self.ctx,
+                                                       fake_assembly,
+                                                       'Heat Stack',
+                                                       'Heat Stack test',
+                                                       'http://fake.ref')
+
+    @mock.patch('solum.common.clients.OpenStackClients')
+    def test_update_assembly_status(self, mock_clients):
+        handler = heat_handler.Handler()
+        fake_assembly = fakes.FakeAssembly()
+        stack = mock.MagicMock()
+        stack.status = 'COMPLETE'
+        mock_clients.heat().stacks.get.return_value = stack
+        handler._parse_server_url = mock.MagicMock(return_value=('xyz'))
+        handler._update_assembly_status(self.ctx, fake_assembly, mock_clients,
+                                        'fake_id')
+        self.assertEqual(fake_assembly.status, 'READY')
+        fake_assembly.save.assert_called_once_with(self.ctx)
+
+    @mock.patch('solum.common.clients.OpenStackClients')
+    def test_update_assembly_status_failed(self, mock_clients):
+        handler = heat_handler.Handler()
+        fake_assembly = fakes.FakeAssembly()
+        stack = mock.MagicMock()
+        stack.status = 'FAILED'
+        mock_clients.heat().stacks.get.return_value = stack
+        handler._update_assembly_status(self.ctx, fake_assembly, mock_clients,
+                                        'fake_id')
+        self.assertEqual(fake_assembly.status, 'ERROR')
+        fake_assembly.save.assert_called_once_with(self.ctx)
+
+    def test_parse_server_url(self):
+        handler = heat_handler.Handler()
+        heat_output = mock.MagicMock()
+        heat_output._info = {"id": "fake_id",
+                             "outputs": [{"output_value": "192.168.78.21",
+                                          "description": "IP", "output_key":
+                                          "public_ip"},
+                                         {"output_value":
+                                          "http://192.168.78.21:5000",
+                                          "description": "URL", "output_key":
+                                          "URL"}]}
+        host_url = handler._parse_server_url(heat_output)
+        self.assertEqual(host_url, "http://192.168.78.21:5000")
