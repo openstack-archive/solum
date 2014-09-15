3c3a51ede6014e6ca4a2810a8da4675cc3a7fb18
Dimitry Ushakov <dimitry.ushakov@rackspace.com>
2014-09-15 16:57:28 -0500

    Added tests for bug-1367470
    Change-Id: If862b4c412f031cfaf6e4bb4245930d0ce1a532e
    Related-Bug: #1367470

--- functionaltests/api/test_versions.py
+++ functionaltests/api/test_versions.py
@@ -30,3 +30,60 @@ class VersionDiscoveryTestCase(base.TestCase):
         self.assertEqual(v1['status'], 'CURRENT')
         self.assertEqual(v1['link']['target_name'], 'v1')
         self.assertEqual(v1['link']['href'], '%s/v1' % self.client.base_url)
+
+    def test_delete_root_discovers_v1(self):
+        resp, body = self.client.delete('/')
+        body = json.loads(body)
+        self.assertEqual(resp.status, 200)
+        self.assertEqual(len(body), 1)
+        v1 = body[0]
+        self.assertEqual(v1['id'], 'v1.0')
+        self.assertEqual(v1['status'], 'CURRENT')
+        self.assertEqual(v1['link']['target_name'], 'v1')
+        self.assertEqual(v1['link']['href'], '%s/v1' % self.client.base_url)
+
+    def test_post_root_discovers_v1(self):
+        resp, body = self.client.post('/', '{}')
+        body = json.loads(body)
+        self.assertEqual(resp.status, 200)
+        self.assertEqual(len(body), 1)
+        v1 = body[0]
+        self.assertEqual(v1['id'], 'v1.0')
+        self.assertEqual(v1['status'], 'CURRENT')
+        self.assertEqual(v1['link']['target_name'], 'v1')
+        self.assertEqual(v1['link']['href'], '%s/v1' % self.client.base_url)
+
+    def test_put_root_discovers_v1(self):
+        resp, body = self.client.put('/', '{}')
+        body = json.loads(body)
+        self.assertEqual(resp.status, 200)
+        self.assertEqual(len(body), 1)
+        v1 = body[0]
+        self.assertEqual(v1['id'], 'v1.0')
+        self.assertEqual(v1['status'], 'CURRENT')
+        self.assertEqual(v1['link']['target_name'], 'v1')
+        self.assertEqual(v1['link']['href'], '%s/v1' % self.client.base_url)
+
+    def test_post_no_body_root_discovers_v1(self):
+        self.skipTest("POST without body will hang request: #1367470")
+        resp, body = self.client.post('/', None)
+        body = json.loads(body)
+        self.assertEqual(resp.status, 200)
+        self.assertEqual(len(body), 1)
+        v1 = body[0]
+        self.assertEqual(v1['id'], 'v1.0')
+        self.assertEqual(v1['status'], 'CURRENT')
+        self.assertEqual(v1['link']['target_name'], 'v1')
+        self.assertEqual(v1['link']['href'], '%s/v1' % self.client.base_url)
+
+    def test_put_no_body_root_discovers_v1(self):
+        self.skipTest("PUT without body will hang request: #1367470")
+        resp, body = self.client.put('/', None)
+        body = json.loads(body)
+        self.assertEqual(resp.status, 200)
+        self.assertEqual(len(body), 1)
+        v1 = body[0]
+        self.assertEqual(v1['id'], 'v1.0')
+        self.assertEqual(v1['status'], 'CURRENT')
+        self.assertEqual(v1['link']['target_name'], 'v1')
+        self.assertEqual(v1['link']['href'], '%s/v1' % self.client.base_url)
