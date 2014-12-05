74d8fb6c6cabd5681819a1ebb0306a966a46915f
Jeremy Stanley <fungi@yuggoth.org>
2014-12-05 03:30:46 +0000

    Workflow documentation is now in infra-manual
    Replace URLs for workflow documentation to appropriate parts of the
    OpenStack Project Infrastructure Manual.
    
    Change-Id: I60c82c0ae8d4fb7bddabd61d755049402da07126

--- contrib/devstack/setup_devstack.sh
+++ contrib/devstack/setup_devstack.sh
@@ -15,7 +15,7 @@ git clone git://git.openstack.org/stackforge/solum ${SOLUM_DIR}
 
 echo "Getting Devstack Code"
 mkdir -p ${DEVSTACK_DIR}
-git clone https://github.com/openstack-dev/devstack.git ${DEVSTACK_DIR}
+git clone https://git.openstack.org/cgit/openstack-dev/devstack.git ${DEVSTACK_DIR}
 
 echo "Setting up Devstack for Solum"
 cd ${SOLUM_DIR}/contrib/devstack
