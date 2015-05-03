c5cdbeced11099095d5539d567af06f55e378805
James Li <yueli.m@gmail.com>
2015-05-03 04:17:35 +0000

    tempest drops auth
    Change-Id: I52ef8be9693b4a07259688ea22b5b1cbce1c8d8d

--- functionaltests/api/base.py
+++ functionaltests/api/base.py
@@ -19,9 +19,9 @@ import json
 import os
 import time
 
-from tempest import auth
 from tempest import config
 from tempest import manager
+from tempest_lib import auth
 from tempest_lib.common import http
 from tempest_lib.common import rest_client
 import testtools
