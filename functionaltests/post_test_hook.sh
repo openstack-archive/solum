9d98ecfe86e250ab9c089174ed404a51ad8a2806
Devdatta Kulkarni <devdatta.kulkarni@rackspace.com>
2015-10-20 18:08:36 -0500

    Upgrading tempest-lib to unblock dvsm gate
    tempest-lib has changed signature on one of the methods.
    The changed method signature is available in tempest-lib-0.11.0.
    We cannot wait till that version makes it to global-req.
    So installing that version in post_test_hook.sh.
    
    Also, it seems that we need to setup the tempest.conf file
    as required otherwise tempest uses default values, which are not
    appropriate for our testing. So setting up tempest.conf as well
    in post_test_hook.sh
    
    Closes-Bug: #1509407
    Change-Id: I4733604af4effe904bbadca5e9083d03fc082d93

--- functionaltests/post_test_hook.sh
+++ functionaltests/post_test_hook.sh
@@ -17,5 +17,36 @@
 # Install packages from test-requirements.txt
 sudo pip install -r /opt/stack/new/solum/test-requirements.txt
 
+sudo pip uninstall tempest-lib
+
+sudo pip install -U tempest-lib
+
+sudo pip freeze
+
+# Generate tempest.conf file
+TEMPEST_BASE=/opt/stack/new/tempest/
+cd $TEMPEST_BASE
+sudo tox -egenconfig
+sudo mkdir -p /etc/tempest
+sudo cp $TEMPEST_BASE/etc/tempest.conf.sample /etc/tempest/tempest.conf
+
+# Set test parameters
+sudo sed -i s'/#username = <None>/username = solum_user_a/'g /etc/tempest/tempest.conf
+sudo sed -i s'/#tenant_name = <None>/tenant_name = solum_tenant_a/'g /etc/tempest/tempest.conf
+sudo sed -i s'/#password = <None>/password = solum/'g /etc/tempest/tempest.conf
+sudo sed -i s'/#auth_version = <None>/auth_version = v2/'g /etc/tempest/tempest.conf
+sudo sed -i s'/#admin_domain_name = <None>/admin_domain_name = Default/'g /etc/tempest/tempest.conf
+sudo sed -i s'/#admin_tenant_id = <None>/admin_tenant_id = 22c3991d53eb42cbaff28428b90760c2/'g /etc/tempest/tempest.conf
+sudo sed -i s'/#admin_tenant_name = <None>/admin_tenant_name = admin/'g /etc/tempest/tempest.conf
+sudo sed -i s'/#admin_password = <None>/admin_password = solum/'g /etc/tempest/tempest.conf
+sudo sed -i s'/#admin_username = <None>/admin_username = admin/'g /etc/tempest/tempest.conf
+sudo sed -i s'/#alt_tenant_name = <None>/alt_tenant_name = alt_demo/'g /etc/tempest/tempest.conf
+sudo sed -i s'/#alt_password = <None>/alt_password = solum/'g /etc/tempest/tempest.conf
+sudo sed -i s'/#alt_username = <None>/alt_username = alt_demo/'g /etc/tempest/tempest.conf
+sudo sed -i s'/#uri_v3 = <None>/uri_v3 = http\:\/\/127.0.0.1\:5000\/v3/'g /etc/tempest/tempest.conf
+sudo sed -i s'/#uri = <None>/uri = http\:\/\/127.0.0.1\:5000\/v2.0\//'g /etc/tempest/tempest.conf
+
+sudo more /etc/tempest/tempest.conf
+
 cd /opt/stack/new/solum/functionaltests
 sudo ./run_tests.sh
