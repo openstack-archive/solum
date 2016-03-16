78dfeed248caceca817dcccf36a0fd8c28b3e4ca
Devdatta Kulkarni <devdatta.kulkarni@rackspace.com>
2016-03-16 14:44:59 -0500

    Adding https transport for apt to devstack plugin
    Change-Id: I43b3579b5cc23b0485bb07531937752e088d832f

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -366,6 +366,7 @@ if is_service_enabled solum-api solum-conductor solum-deployer solum-worker; the
     if [ ! -f /usr/bin/docker ] ; then
        echo deb http://get.docker.com/ubuntu docker main | sudo tee /etc/apt/sources.list.d/docker.list
        sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+       sudo apt-get install -y apt-transport-https
        sudo apt-get update
        sudo apt-get install -y lxc-docker-1.7.0
     fi
