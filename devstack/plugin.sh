23daae3ee733acd2d1e66b8cf8cb5a9d844e9147
zhurong <aaronzhu1121@gmail.com>
2017-01-03 20:34:34 +0800

    Remove the commenting out config generation TODO
    This patch will revert the changes made to fix the bug[1]
    which was a workaround.
    
    [1] https://bugs.launchpad.net/solum/+bug/1555788
    
    Change-Id: I05a754c670443d0de4969f56c2e7e4137345cfb7

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -86,13 +86,10 @@ function configure_solum() {
     # Generate sample config and configure common parameters.
     mkdir -p /tmp/solum
 
-    # TODO(devkulkarni): Commenting out config generation for now to get around following bug
-    # https://bugs.launchpad.net/solum/+bug/1555788. Instead, including a sample config in the repo
-    #pushd $SOLUM_DIR
-    #    oslo-config-generator --config-file=${SOLUM_DIR}/etc/solum/config-generator.conf --output-file=/tmp/solum/solum.conf.sample
-    #popd
-    #cp /tmp/solum/solum.conf.sample $SOLUM_CONF_DIR/$SOLUM_CONF_FILE
-    cp ${SOLUM_DIR}/devstack/solum.conf.sample $SOLUM_CONF_DIR/$SOLUM_CONF_FILE
+    pushd $SOLUM_DIR
+        oslo-config-generator --config-file=${SOLUM_DIR}/etc/solum/config-generator.conf --output-file=/tmp/solum/solum.conf.sample
+    popd
+    cp /tmp/solum/solum.conf.sample $SOLUM_CONF_DIR/$SOLUM_CONF_FILE
 
     iniset $SOLUM_CONF_DIR/$SOLUM_CONF_FILE DEFAULT debug $SOLUM_DEBUG
 
