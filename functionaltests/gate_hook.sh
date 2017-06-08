1e658701dd64fb4adb7e32a40f9a8ebcf151defb
zhurong <aaronzhu1121@gmail.com>
2017-06-08 13:55:38 +0800

    Remove devstack-plugin-container from gate_hook
    Since we have add devstack-plugin-container to jenkins jobs in [0],
    So we need remove it from gate_hook.
    
    [0]: I0bbd654499ec3a916766352f0cda64e78ae6109d
    
    Change-Id: I1c3272b16065e85793748b4c201ab68408c715e2

--- functionaltests/gate_hook.sh
+++ functionaltests/gate_hook.sh
@@ -18,4 +18,3 @@
 export REQUIREMENTS_MODE=soft
 
 $BASE/new/devstack-gate/devstack-vm-gate.sh
-export DEVSTACK_LOCAL_CONFIG+=$'\n'"enable_plugin devstack-plugin-container https://git.openstack.org/openstack/devstack-plugin-container"
