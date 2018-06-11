4bb3f91e8afc1e60f674d3e191a5945dfe909bd2
zhurong <aaronzhu1121@gmail.com>
2018-06-11 20:57:58 +0800

    Remove pycrypto dependency
    Change-Id: I49f912974840c7afe516bc451e1f1d4ba72c7479
    Closes-Bug: #1749574

--- solum/common/utils.py
+++ solum/common/utils.py
@@ -10,51 +10,25 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from cryptography.fernet import Fernet
 
-from Crypto.Cipher import AES
 from oslo_concurrency import processutils
 from oslo_config import cfg
+from oslo_utils import encodeutils
 
 
-SERVICE_OPTS = [
-    cfg.StrOpt('encryption_key',
-               default='This is a key123',
-               help=('The secret key to use in the symmetric cipher. '
-                     'It must be 16 (AES-128), 24 (AES-192), '
-                     'or 32 (AES-256) bytes long.')),
-    cfg.StrOpt('initialization_vector',
-               default='This is an IV456',
-               help=('The initialization vector to use for encryption '
-                     'or decryption. ')),
-]
-
-opt_group = cfg.OptGroup(
-    name='encryption',
-    title='Options for Encryption')
-
-
-def list_opts():
-    yield opt_group, SERVICE_OPTS
-
-
-cfg.CONF.register_group(opt_group)
-cfg.CONF.register_opts(SERVICE_OPTS, opt_group)
+key = Fernet.generate_key()
+cipher_suite = Fernet(key)
 
 
 def encrypt(value):
-    encryption_key = cfg.CONF.encryption.encryption_key
-    init_vector = cfg.CONF.encryption.initialization_vector
-    obj = AES.new(encryption_key, AES.MODE_CFB, init_vector)
-    ciphertext = obj.encrypt(value)
+    ciphertext = cipher_suite.encrypt(encodeutils.safe_encode(value))
     return ciphertext
 
 
 def decrypt(ciphertext):
-    encryption_key = cfg.CONF.encryption.encryption_key
-    init_vector = cfg.CONF.encryption.initialization_vector
-    obj = AES.new(encryption_key, AES.MODE_CFB, init_vector)
-    value = obj.decrypt(ciphertext)
-    return value
+    value = cipher_suite.decrypt(ciphertext)
+    return encodeutils.safe_decode(value, 'utf-8')
 
 
 def get_root_helper():
