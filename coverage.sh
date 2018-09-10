c38d9bd2d2d33ce9f40bcf87b359d0e234ff38bf
zhurong <aaronzhu1121@gmail.com>
2018-09-10 11:01:09 +0800

    Use standard cover tox env
    Change-Id: I4aff1415ef0bbe83ea043388e13e2384b372c7d7

--- coverage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-args=
-if [ -n "$@" ] ; then
-    args="-t $@"
-fi
-python setup.py testr --coverage --slowest "$args"
-python -m coverage report --show-missing
-echo "Coverage generated, see cover/index.html"
